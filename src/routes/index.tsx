18a57bb2356441cb8c50d74bd69c04d5daf2ffdd
Georgi Parlakov <gparlakov@gmail.com>
2023-07-08 14:19:04 +0300

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import { Link, type DocumentHead } from '@builder.io/qwik-city';
 export default component$(() => {
   return (
     <>
-      <Link href="/graphql">GraphQL UI</Link>
-       <Link href="/page">Page making requests</Link>
+      <Link href="/graphql">GraphQL UI</Link> |
+      <Link href="/page">Page making requests</Link>
     </>
   );
 });
