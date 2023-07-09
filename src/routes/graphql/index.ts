0666614ad623d4a7af5d543946711ca8477c5816
Georgi Parlakov <gparlakov@gmail.com>
2023-07-09 11:54:25 +0300

    feat: init (no routes)

--- src/routes/graphql/index.ts
+++
@@ -1,5 +0,0 @@
-import { RequestHandler } from '@builder.io/qwik-city'
-
-export const onRequest: RequestHandler = (({send}) => {
-    send(200, 'Success')
-})
--- src/routes/page/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-
-export default component$(() => {
-    return (
-      <>
-        // make request to /graphql and display results
-      </>
-    );
-  });
